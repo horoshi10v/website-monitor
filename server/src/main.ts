467810a980e970d86477abb516cfc53fd61eb445
Alexandru Tostogan <otostogan2020@gmail.com>
2025-01-06 15:07:54 +0100

    another way of cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,13 @@ import { WebsitesModule } from './websites/websites.module';
 import * as process from "node:process";
 
 async function bootstrap() {
-  const app = await NestFactory.create(WebsitesModule);
+  const app = await NestFactory.create(WebsitesModule, { cors: true });
 
-  app.enableCors({
-    origin: process.env.CLIENT_URL ?? 'http://localhost:3000',
-    methods: 'GET,POST',
-    credentials: true,
-  });
+//   app.enableCors({
+//     origin: process.env.CLIENT_URL ?? 'http://localhost:3000',
+//     methods: 'GET,POST',
+//     credentials: true,
+//   });
 
   await app.listen(process.env.PORT ?? 3002);
 }
